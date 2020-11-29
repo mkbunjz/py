b303f53b8a1946084c88ba0ccd02292b7fa1b10b
Han Tun Zaw <37868165+mkbunjz@users.noreply.github.com>
2020-11-30 03:21:32 +0630

    added some New Code

---
+++ hello.py
@@ -0,0 +1,9 @@
+a = input("Name :")
+b = int(input("Age : "))
+
+if b > 18:
+    print ("Welcome Form Python")   
+elif b < 18:
+    print ("You Cannot Use Python Yet")
+else:
+    print("Try Again")
