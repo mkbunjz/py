101736504b3bdbb21a9d30ff6f69eb438a4cad39
Han Tun Zaw <37868165+mkbunjz@users.noreply.github.com>
2020-11-30 03:48:45 +0630

    added Set lists

--- hello.py
+++ hello.py
@@ -10,3 +10,18 @@ number.append(100)
 number.count(100)
 number.sort()
 print(number)
+
+#set Example
+
+h = set()
+
+h.add("Han Tun Zaw")
+h.add(1)
+h.add(20)
+h.add(50)
+h.add(60)
+h.add(70)
+h.add("May Kyi Nue")
+h.remove("Han Tun Zaw")
+print(h)
+print(f"Here Is The Length Of H {len(h)}")
